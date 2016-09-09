9a32ecf5f98e4b9b4284a5e584c994ec65ec08bd
Admin <Admin@ADMIN-PC>
2016-09-09 10:41:26 -0700

    Add project files.

---
+++ LupMVC5App.UI/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using LupMVC5App.UI.Infrastructure;
+
+namespace LupMVC5App.UI.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LupMVC5App.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace LupMVC5App.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
