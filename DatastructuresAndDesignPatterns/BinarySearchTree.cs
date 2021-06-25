17ac08d1a7a61d184eafe4ecd5f3d051a1fab9cc
Rishirakish <rishiroser@gmail.com>
2021-06-25 20:09:04 +0530

    Binary search tree

---
+++ DatastructuresAndDesignPatterns/BinarySearchTree.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndDesignPatterns
+{
+    public class BinarySearchTree
+    {
+        public class Node
+        {
+            public int Data;
+            public Node left;
+            public Node right;
+        }
+
+        public Node rootNode;
+        public void Display(Node root)
+        {
+            if (root != null)
+                Console.Write(" " + root.Data);
+
+            if(root.left != null)
+            {
+                Display(root.left);
+            }
+
+            if(root.right != null)
+            {
+                Display(root.right);
+            }
+        }
+
+        public void InsertMethod1(int value)
+        {
+            Node newNode = new Node();
+            newNode.Data = value;
+            if (rootNode == null)
+                rootNode = newNode;
+
+            else
+            {
+                Node parent = rootNode;
+                
+                while(parent != null)
+                {
+                    if(parent.Data > value)
+                    {
+                        if (parent.left != null)
+                            parent = parent.left;
+                        else
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (parent.right != null)
+                            parent = parent.right;
+                        else
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void InsertMethod2(int value)
+        {
+            Node newNode = new Node();
+            newNode.Data = value;
+            if (rootNode == null)
+                rootNode = newNode;
+
+            else
+            {
+                Node parent = rootNode;
+
+                while (parent != null)
+                {
+                    if (parent.Data > value)
+                    {
+                        if (parent.left != null)
+                            parent = parent.left;
+                        else
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (parent.right != null)
+                            parent = parent.right;
+                        else
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Question: A program to check if a binary tree is BST or not
+        public bool isBST(Node root, Node l, Node r)
+        {
+            if(root != null)
+            {
+                if(root.left != null)
+                {
+                    if (root.Data <= l.Data)
+                        return false;
+                    isBST(root, l.left, r.right);
+                }
+                if (root.right != null)
+                {
+                    if (root.Data >= r.Data)
+                        return false;
+                    isBST(root.left, r.left, r.right);
+                }
+            }
+            return true;
+        }
+    }
+}
--- DatastructuresAndDesignPatterns/Program.cs
+++ DatastructuresAndDesignPatterns/Program.cs
@@ -8,8 +8,31 @@ namespace DatastructuresAndDesignPatterns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            RemoveDuplicateFromString obj = new RemoveDuplicateFromString();
-            Console.WriteLine(obj.RemoveDuplicateCharMethod2("HappyNewYear"));
+            //RemoveDuplicateFromString obj = new RemoveDuplicateFromString();
+            //Console.WriteLine(obj.RemoveDuplicateCharMethod2("HappyNewYear"));
+
+            BinarySearchTree bst = new BinarySearchTree();
+            //bst.InsertMethod2(5);
+            //bst.InsertMethod2(4);
+            //bst.InsertMethod2(7);
+            //bst.InsertMethod2(6);
+            //bst.Display(bst.rootNode);
+
+            bst.rootNode = new BinarySearchTree.Node();
+            bst.rootNode.Data = 3;
+            bst.rootNode.left = new BinarySearchTree.Node();
+            bst.rootNode.left.Data = 2;
+            bst.rootNode.right = new BinarySearchTree.Node();
+            bst.rootNode.right.Data = 5;
+
+            bst.rootNode.left.left = new BinarySearchTree.Node();
+            bst.rootNode.left.left.Data = 1;
+            bst.rootNode.left.right = new BinarySearchTree.Node();
+            bst.rootNode.left.right.Data = 4;
+
+            bst.Display(bst.rootNode);
+
+            Console.WriteLine(bst.isBST(bst.rootNode));
         }
     }
 }
